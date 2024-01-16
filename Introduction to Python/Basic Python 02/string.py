dc46c1e29936ec31e91b9c1575bf5eac4665fc07
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-01-16 23:29:33 +0600

    3-1

---
+++ Introduction to Python/Basic Python 02/string.py
@@ -0,0 +1,22 @@
+str = "Hello, User!"
+print(f"The string: {str}")
+
+first_char = str[0]
+last_char = str[-1]
+print(f"First character: {first_char}")
+print(f"Last character: {last_char}")
+
+# concatenating
+greeting = "Hello"
+name = "MIC"
+full_greet = greeting + ", " + name + "!"
+print(full_greet)
+
+# some methods
+upper = str.upper()
+lower = str.lower()
+length = len(str)
+print(upper)
+print(lower)
+print(length)
+
