d2af701ee0924c9a06280c32e6cdd190f540e78d
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-01-17 23:01:46 +0600

    lambda

---
+++ Introduction to Python/Basic Python 02/lamda_function.py
@@ -0,0 +1,12 @@
+# lambda arguments: expression
+
+#normal function
+def square(x):
+    return x**2
+
+print(square(5))
+
+# lambda function
+square_lambda = lambda x: x**2
+
+print(square_lambda(5))
