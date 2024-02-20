c5b82b85aadcf3c913b0e5bfda042667bda89ac9
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-02-20 16:34:15 +0600

    inheritance

---
+++ OOP/Python OOP/inheritance.py
@@ -0,0 +1,32 @@
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def make_sound(self):
+        print (f"{self.name} says {self.sound}!")
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Meowwww")
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Wooouuuuuf")
+
+class Bird(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Tweeet")
+
+    def fly(self):
+        print(f"{self.name} is flying.")
+
+
+cat = Cat("Micky")
+dog = Dog("Bob")
+bird = Bird("Poi")
+
+cat.make_sound()
+dog.make_sound()
+bird.fly()
+
