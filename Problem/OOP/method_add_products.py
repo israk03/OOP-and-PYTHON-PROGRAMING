a6844c71648b7d001ae5890debccd3e05ee98339
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-03-16 02:38:01 +0600

    create a method name add_product

---
+++ Problem/OOP/method_add_products.py
@@ -0,0 +1,34 @@
+# Inside the Shop class, create a method name add_product which adds products using the Product class to the Shop class.
+
+# create a product class and a shop class
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+class Shop:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    """def list_products(self):
+        if not self.products:
+            print("No product available.")
+        else:
+            print("Products available in", self.name + ":")
+            for p in self.products:
+                print("-", p.name, "(TK.", p.price, ")")"""
+
+    def add_product(self, product):
+        self.products.append(product)
+        print(f"{product.name} added to {self.name}.")
+
+p1 = Product("Laptop", 52000)
+p2 = Product("iPhone", 36000)
+
+my_shop = Shop("My Shop")
+
+my_shop.add_product(p1)
+my_shop.add_product(p2)
+
+#my_shop.list_products()
