2cdbe3482792cd20e73a08e23a805fee970c8aca
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-03-16 03:33:26 +0600

    overriding

---
+++ Problem/OOP/overriding.py
@@ -0,0 +1,19 @@
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def speak(self):
+        print("Cat meows")
+
+# Creating instances of Dog and Cat
+dog = Dog()
+cat = Cat()
+
+# Calling the speak method of each instance
+dog.speak()  # Output: Dog barks
+cat.speak()  # Output: Cat meows
